a794664300be434430450dd0c477f8b407829185
ziko-23 <kabouchziko3@gmail.com>
2023-02-22 08:45:17 +0100

    app

---
+++ src/main/java/com/app/Customer.java
@@ -0,0 +1,74 @@
+package com.app;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(name = "customer_id_sequance",sequenceName = "customer_id_sequance",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "customer_id_sequance")
+    private Integer id;
+    private String name;
+    private int age;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && age == customer.age && Objects.equals(name, customer.name) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Customer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/main/java/com/app/Main.java
@@ -0,0 +1,38 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("api/v1/customers")
+public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+    record NewCustomerRequest(String name,int age){
+
+    }
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer(request.name(), request.age());
+        customerRepository.save(customer);
+    }
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+}
